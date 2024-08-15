126afba9fa91ea581f2d66d1f084c5330a2b9e94
sanjayprasad s <sanjuhs123@gmail.com>
2024-08-15 23:42:07 +0530

    first commit2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ export default function Home() {
   return (
     <main className="">
       <p>Hellow world </p>
+      <p> ok lets span this and lets figure out what we need to put here</p>
+
+      <div className="navbar flex">
+        <div className="left section">logo + Name ( weight-scribe LLMs)</div>
+      </div>
     </main>
   );
 }
